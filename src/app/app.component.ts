3f056f4e8580917d310483268f0adf4fc157fa64
jehumtine <jehumtine16@gmail.com>
2025-05-13 12:27:08 +0200

    to production

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './shared/navbar/navbar.component';
 import {FooterComponent} from './shared/footer/footer.component';
+import {MobileBlockComponent} from './mobile-block/mobile-block.component';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, MobileBlockComponent,    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'jehumtine';
+  isMobile = false;
+
+  ngOnInit(): void {
+    const userAgent = navigator.userAgent || navigator.vendor;
+
+    this.isMobile = /android|iphone|ipad|mobile/i.test(userAgent);
+  }
 }
